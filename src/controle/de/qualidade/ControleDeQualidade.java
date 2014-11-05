d66e05c3bce7429c9e42560fc98f9363497e90e9
Davy Souza <davy.souza@outlook.com>
2014-11-05 21:46:38 -0200

    Primeiro comiitittt

---
+++ src/controle/de/qualidade/ControleDeQualidade.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle.de.qualidade;
+
+/**
+ *
+ * @author Bruno
+ */
+public class ControleDeQualidade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaInicial tela = new TelaInicial();
+        tela.setVisible(true);
+    }
+    
+}
